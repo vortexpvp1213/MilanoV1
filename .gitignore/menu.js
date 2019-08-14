ee3748d9f5f1e274abdd462f098a7cd872403b82
ToToChti <53660084+ToToChti@users.noreply.github.com>
2019-08-14 18:11:27 +0200

    Update menu.js

--- .gitignore/menu.js
+++ .gitignore/menu.js
@@ -829,7 +829,7 @@ message.channel.send("✅ Successfully reloved")
         message.channel.send(m.user.username + " is mute ? => " + m.mute)
       })
     }
-if(message.content === prefix + "jj")
+if(message.content === prefix + "jj") {
   message.channel.send(message.attachments.forEach(fi => fi.url).join(" - "))
 }
 
